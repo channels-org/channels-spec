f28368d0696edfeaa7b52747d2a0e5252c56915a
Evgeny Poberezkin <e.poberezkin@me.com>
2019-07-13 22:47:35 +0100

    schema: channel

--- schema/tests/schema.spec.js
+++ schema/tests/schema.spec.js
@@ -3,17 +3,42 @@ const ajv = new Ajv({
   strictKeywords: true,
   allErrors: true,
   schemas: [
-    require('../v0.1/defs.json')
+    require('../v0.1/defs.json'),
+    require('../v0.1/patch.json')
   ]
 });
 
 
 describe('schema', () => {
   describe('v0.1', () => {
+    const itemSchema = require('../v0.1/item.json');
+    const changeSchema = require('../v0.1/change.json');
+
     describe('item.json', () => {
       it('should be a valid JSON Schema', () => {
-        ajv.compile(require('../v0.1/item.json'));
+        ajv.compile(itemSchema);
+      });
+    });
+
+    describe('change.json', () => {
+      it('should be a valid JSON Schema', () => {
+        ajv.compile(changeSchema);
+      });
+    });
+
+    describe('channel.json', () => {
+      it('should be a valid JSON Schema', () => {
+        addSchema(itemSchema);
+        addSchema(changeSchema);
+        ajv.compile(require('../v0.1/channel.json'));
       });
     });
   });
 });
+
+
+function addSchema(schema) {
+  if (!ajv.getSchema(schema.$id)) {
+    ajv.addSchema(schema);
+  }
+}
